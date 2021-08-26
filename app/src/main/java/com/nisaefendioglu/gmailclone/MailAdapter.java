5f58487e203bc6ecf7c71a6d8645c9863004c2bf
nisaefendioglu <nisa1517@gmail.com>
2021-08-26 14:57:12 +0300

    Added mailing feature and listview.

---
+++ app/src/main/java/com/nisaefendioglu/gmailclone/MailAdapter.java
@@ -0,0 +1,55 @@
+package com.nisaefendioglu.gmailclone;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MailAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private ArrayList<MailList> mailArrayList;
+
+    public MailAdapter(Activity activity, ArrayList<MailList> padisahArrayList) {
+
+        this.mInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.mailArrayList = padisahArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return mailArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mailArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView = mInflater.inflate(R.layout.mail_list, null);
+        TextView mailName =  convertView.findViewById(R.id.mailName);
+        TextView mailContent = convertView.findViewById(R.id.mailContent);
+        TextView mailNames = convertView.findViewById(R.id.mailNames);
+        ImageView imageView = convertView.findViewById(R.id.imageView);
+        MailList mail = mailArrayList.get(position);
+        mailName.setText(mail.getName());
+        mailContent.setText(mail.getMail());
+        mailNames.setText(mail.getMailNames());
+        imageView.setImageResource(mail.getImage());
+        return convertView;
+    }
+}
--- app/src/main/java/com/nisaefendioglu/gmailclone/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/gmailclone/MainActivity.java
@@ -2,21 +2,26 @@ package com.nisaefendioglu.gmailclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView gmailListView;
+    final ArrayList<MailList> mailList = new ArrayList<MailList>();
     FloatingActionButton sendMessage;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         sendMessage = findViewById(R.id.sendMessage);
@@ -27,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        gmailListView = findViewById(R.id.gmailListView);
+
+        mailList.add(new MailList("Google","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","G",R.menu.circle));
+        mailList.add(new MailList("Nisa","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","N",R.menu.circle2));
+        mailList.add(new MailList("Mehmet","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","M",R.menu.circle3));
+        mailList.add(new MailList("Hülya","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","H",R.menu.circle4));
+        mailList.add(new MailList("Ayşe","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","A",R.menu.circle5));
+        mailList.add(new MailList("Semih","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","S",R.menu.circle));
+        mailList.add(new MailList("Ece","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","E",R.menu.circle2));
+        mailList.add(new MailList("Karsu","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).","K",R.menu.circle3));
+
+
+
+
+        MailAdapter mailAdapter = new MailAdapter(this, mailList);
+        if (gmailListView != null) {
+            gmailListView.setAdapter(mailAdapter);
+        }
+
     }
 }
