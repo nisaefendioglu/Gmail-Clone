407d7863eeb484465aae19131bc941bef85a4a4c
nisaefendioglu <nisa1517@gmail.com>
2021-08-26 11:15:28 +0300

    design edited.

--- app/src/main/java/com/nisaefendioglu/gmailclone/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/gmailclone/MainActivity.java
@@ -2,13 +2,22 @@ package com.nisaefendioglu.gmailclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton added;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        added = findViewById(R.id.added);
+        added.setBackgroundTintMode(null);
     }
 }
